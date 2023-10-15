a713511456be18cf973ba33f58758c7cbe439526
shjshshsjsu <122176104+shjshshsjsu@users.noreply.github.com>
2023-10-15 22:44:56 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -14,7 +14,7 @@ local StatusClickLabel = Instance.new("TextLabel")
 local closebutton = Instance.new("TextButton")
 
 main.Name = "main"
-main.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+main.Parent = game.Players.LocalPlayer:WaitForChild("ScreenGui")
 main.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 main.ResetOnSpawn = false
 main.IgnoreGuiInset = true -- Ekran kenarlarını dikkate alma
