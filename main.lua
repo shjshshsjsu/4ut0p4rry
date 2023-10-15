ef09f7643526385d9fb49dd2a1b4a1d8418b2cbc
shjshshsjsu <122176104+shjshshsjsu@users.noreply.github.com>
2023-10-15 13:25:55 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,13 @@
+local main = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
 
+main.Name = "main"
+main.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+main.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+main.ResetOnSpawn = false 
+
+Frame.Parent = main
+Frame.BackgroundColor3 = Color3.fromRGB(54, 54, 54)
+Frame.BorderColor3 = Color3.fromRGB(103, 221, 213)
+Frame.Position = UDim2.new(0.100320168, 0, 0.379746825, 0)
+Frame.Size = UDim2.new(0, 190, 0, 57) 
