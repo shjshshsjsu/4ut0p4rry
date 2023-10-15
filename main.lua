943a1c5f1f344794149e1f7dd4acdca844068da4
shjshshsjsu <122176104+shjshshsjsu@users.noreply.github.com>
2023-10-15 14:05:12 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -34,18 +34,28 @@ Duration = 5;
 -- Auto Parry
 
 Auto.MouseButton1Click:connect(function()
-    getgenv().god = true  
-while getgenv().god do
-    for _,ball in next, workspace.Balls:GetChildren() do
-        if ball then
-            if game:GetService("Players").LocalPlayer.Character and game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
-                game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position, ball.Position)
-                if game:GetService("Players").LocalPlayer.Character:FindFirstChild("Highlight") then
-                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = ball.CFrame * CFrame.new(0, 0, (ball.Velocity).Magnitude * -0.5)
-                    game:GetService("ReplicatedStorage").Remotes.ParryButtonPress:Fire()
+    getgenv().god = true
+
+    while getgenv().god do
+        task.wait(1)
+
+        local localPlayer = game:GetService("Players").LocalPlayer
+        local character = localPlayer.Character
+        local humanoidRootPart = character and character:FindFirstChild("HumanoidRootPart")
+
+        if humanoidRootPart then
+            local balls = workspace.Balls:GetChildren()
+
+            for _, ball in ipairs(balls) do
+                if ball then
+                    humanoidRootPart.CFrame = CFrame.new(humanoidRootPart.Position, ball.Position)
+
+                    local highlight = character:FindFirstChild("Highlight")
+                    if highlight then
+                        game:GetService("ReplicatedStorage").Remotes.ParryButtonPress:Fire()
+                    end
                 end
             end
         end
     end
-            end
 end)
