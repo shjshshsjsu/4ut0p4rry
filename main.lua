8cc9d3c558b48eec1c6e9a81b401c8d7b2e36a14
shjshshsjsu <122176104+shjshshsjsu@users.noreply.github.com>
2023-10-15 13:48:07 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -30,3 +30,22 @@ Title = "Blade Ball V1";
 Text = "By Wreston";
 Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
 Duration = 5; 
+
+-- Auto Parry
+
+Auto.MouseButton1Down:connect(function()
+getgenv().god = true
+while getgenv().god and task.wait() do
+    for _,ball in next, workspace.Balls:GetChildren() do
+        if ball then
+            if game:GetService("Players").LocalPlayer.Character and game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+                game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position, ball.Position)
+                if game:GetService("Players").LocalPlayer.Character:FindFirstChild("Highlight") then
+                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = ball.CFrame * CFrame.new(0, 0, (ball.Velocity).Magnitude * -0.5)
+                    game:GetService("ReplicatedStorage").Remotes.ParryButtonPress:Fire()
+                end
+            end
+        end
+    end
+    end
+  end
