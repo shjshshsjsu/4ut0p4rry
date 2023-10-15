739d34d63498951c16b8c0d69c0ab090e290c958
shjshshsjsu <122176104+shjshshsjsu@users.noreply.github.com>
2023-10-15 22:06:52 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,40 @@
+local dragging
+local dragInput
+local dragStart
+local startPos
+
+-- Sürükleme Fonksiyonları
+local function updateInput(input)
+    local delta = input.Position - dragStart
+    Frame.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+end
+
+Frame.InputBegan:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+        dragging = true
+        dragStart = input.Position
+        startPos = Frame.Position
+
+        input.Changed:Connect(function()
+            if input.UserInputState == Enum.UserInputState.End then
+                dragging = false
+            end
+        end)
+    end
+end)
+
+Frame.InputChanged:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+        dragInput = input
+    end
+end)
+
+game:GetService("UserInputService").InputChanged:Connect(function(input)
+    if input == dragInput and dragging then
+        updateInput(input)
+    end
+end)
+
 local main = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
 local AutoParry = Instance.new("TextButton")
@@ -5,6 +42,8 @@ local StatusParryLabel = Instance.new("TextLabel")
 local AutoClick = Instance.new("TextButton")
 local StatusClickLabel = Instance.new("TextLabel")
 local closebutton = Instance.new("TextButton")
+
+
 
 main.Name = "main"
 main.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
